5dbda5bd5cc78a374fa802cce824b37ff48622a4
Jermi Emanuel Pinto Patzan <jpintop@miumg.edu.gt>
2026-08-27 18:49:54 -0600

    Se agregan archivo index  dentro de nuestro src

---
+++ src/dominio/index.ts
@@ -0,0 +1,7 @@
+/** Barril de exportación del núcleo de dominio (E4). */
+export * from './dinero';
+export * from './plan-amortizacion';
+export * from './calculadora-mora';
+export * from './prelacion-pago';
+export * from './cartera';
+export * from './estado-credito';
